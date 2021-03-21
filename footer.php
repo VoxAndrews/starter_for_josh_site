27105831c1e558452671c4373bea888716220c60
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-03-21 19:01:28 +0800

    Added the PHP Files
    Added footer, functions, header, index, screenshots PHP

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+                <p> <?php bloginfo('name'); ?></p>
+                <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt="Tumblr Account Link">
+                <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt="RSS Account Link">
+            </div>
+            <!--Column Medium 6-->
+
+            <div class="col-md-6 text-center footer-content">
+                <p> Terms and Conditions </p>
+                <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt="LinkedIn Account Link">
+                <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt="Facebook Account Link">
+            </div>
+            <!--Column Medium 6-->
+        </div>
+        <!--Row-->
+    </div>
+    <!--Container-->
+</footer>
+<!--Fluid Container-->
+<?php wp_footer(); ?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            <h2> GAMES &amp; LEVEL DESIGNER </h2>
+            <p class="particle"> ...and particle wizard </p>
+
+            <p class="about-text"> My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story </p>
+
+            <div class="blocker"></div>
+        </div>
+        <!--Container-->
+    </section>
+    <!--Fluid Container-->
+
+    <section class="container-fluid articlebg">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h3 class="text-center article-title"> LATEST ARTICLES </h3>
+                </div>
+
+                <div class="col-md-4 article-cent">
+                    <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub4/wpcontent/themes/starter_for_josh_site/images/game.jpg" alt="Fantasy Portrait of Female Bowman">
+
+                    <h4> A PORTRAIT </h4>
+                    <p class="date"> Friday, November 10. 2019</p>
+
+                    <p> STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+
+                    <p class="readmore"> CONTINUE READING </p>
+                </div>
+                <!--Column Medium 4-->
+
+                <div class="col-md-4 article-cent">
+                    <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub4/wpcontent/themes/starter_for_josh_site/images/game.jpg" alt="3D Render of Glowing Robot Suit">
+
+                    <h4> A MODEL </h4>
+                    <p class="date"> Friday, November 10. 2019</p>
+
+                    <p> STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+
+                    <p class="readmore"> CONTINUE READING </p>
+                </div>
+                <!--Column Medium 4-->
+
+                <div class="col-md-4 article-cent">
+                    <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub4/wpcontent/themes/starter_for_josh_site/images/game.jpg" alt="Overhead view of Racing Track">
+
+                    <h4> GAMES DESIGN </h4>
+                    <p class="date"> Friday, November 10. 2019</p>
+
+                    <p> STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+
+                    <p class="readmore"> CONTINUE READING </p>
+                </div>
+                <!--Column Medium 4-->
+            </div>
+            <!--Row-->
+        </div>
+        <!--Container-->
+    </section>
+    <!--Fluid Container-->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
