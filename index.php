51f146ef2f1501383a34d9e945bcba5b01efef0b
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-04-08 13:52:50 +0800

    Update index.php
    Added PHP Code required for Article Post

--- index.php
+++ index.php
@@ -30,14 +30,12 @@ wp_reset_query();?>
                 </div>
 
                 <div class="col-md-4 article-cent">
-                    <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/starter_for_josh_site/images/img-3.jpg" alt="Fantasy Portrait of Female Bowman">
-
-                    <h4> A PORTRAIT </h4>
-                    <p class="date"> Friday, November 10. 2019</p>
-
-                    <p> STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-
-                    <p class="readmore"> CONTINUE READING </p>
+<?php $post_id = 243;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
                 </div>
                 <!--Column Medium 4-->
 
