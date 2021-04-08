6bd11d14cd9a557a2e4a7871e00c9844cab3a6c2
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-04-08 15:57:19 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ add_theme_support( 'post-thumbnails');
 function add_menu_link_class( $atts, $item, $args ) {
 if($args->link_class) {$atts['class'] = $args->link_class;
 }
-    return $atts;
+return $atts;
 }
 add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
 function add_menu_list_item_class($classes, $item, $args) {
@@ -24,4 +24,4 @@ if($args->list_item_class){$classes[] = $args->list_item_class;
                           }return $classes;
     }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
-?>
+ ?>
