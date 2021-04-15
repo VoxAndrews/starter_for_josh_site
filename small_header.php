38ceda9a3845d03b4e9428cc175b69432a42b629
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-04-15 19:19:32 +0800

    Update small_header.php

--- small_header.php
+++ small_header.php
@@ -62,14 +62,14 @@
                     <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
-<ul class="nav navbar-nav navbar-right">
-    <?php wp_nav_menu(array(
-        'menu' => 'Top Menu',
-        'items_wrap'=>'%3$s',
-        'container' => false,
-        'list_item_class' => "nav-item",
-        'link_class' => "nav-link",
-        )); ?>
+<ul class="navbar-nav navbar-right">
+<?php wp_nav menu(array(
+    'menu' => 'Top Menu',
+    'items_wrap' => '%3$s',
+    'container' => false,
+    'list_item_class' => "nav-item",
+    'link_class' => "nav-link",
+));?>
 </ul>
                 </div>
             </div>
