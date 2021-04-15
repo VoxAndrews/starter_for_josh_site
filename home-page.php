04559b1fde61b5e4c6b20a5775a8d46522cdd25d
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-04-15 19:59:59 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,45 @@
+<?php
+    /*
+    Template Name: Home Page
+    */
+?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            
+<!--Placeholder for About Me-->
+
+        <div class="blocker"></div>
+        </div>
+        <!--Container-->
+    </section>
+    <!--Fluid Container-->
+
+    <section class="container-fluid articlebg">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h3 class="text-center article-title"> LATEST ARTICLES </h3>
+                </div>
+
+                <div class="col-md-4 article-cent">
+<!--Placeholder for col1-->
+                </div>
+                <!--Column Medium 4-->
+
+                <div class="col-md-4 article-cent">
+<!--Placeholder for col1-->
+                </div>
+                <!--Column Medium 4-->
+
+                <div class="col-md-4 article-cent">
+<!--Placeholder for col1-->
+                </div>
+                <!--Column Medium 4-->
+            </div>
+            <!--Row-->
+        </div>
+        <!--Container-->
+    </section>
+    <!--Fluid Container-->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
